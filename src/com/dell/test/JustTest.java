cddb547663112b66139b55ed97805b560af0fece
tom_glb <15756217611@163.com>
2020-11-12 21:49:40 +0800

    测试类有个转化list成html面试题的功能

--- src/com/dell/test/JustTest.java
+++ src/com/dell/test/JustTest.java
@@ -4,15 +4,43 @@ import com.dell.Service.BookService;
 import com.dell.pojo.Book;
 import com.dell.pojo.Page;
 
+import java.io.File;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class JustTest {
     public static void main(String[] args) {
-        BookService bookService = new BookService();
-        Page page = new Page<>();
-        page.setCurrentPage(100);
-        Page bookPage = bookService.queryBookListWithPage(page);
-        System.out.println(bookPage);
+        List<String> problem = new ArrayList<>();
+        List<String> answer = new ArrayList<>();
+        StringBuilder temp = new StringBuilder();
+        List<String> list = Arrays.asList("34、写一个单例模式并优化",
+                "/**",
+                " * 方式一：创建private实例，公共方法调用"
+        );
+        for (String s : list) {
+            if (Pattern.matches("\\d+、.*", s)) {//答案汇总
+                if (temp.length()!=0){
+                    answer.add(temp.toString());
+                    temp = new StringBuilder();
+                }
+                problem.add(s);//加下一个问题
+            }else if (problem.size()!=0){
+                temp.append(s).append("\n");//答案积累
+            }
+        }
+        if (temp.length()!=0){
+            answer.add(temp.toString());
+        }
+        for (int i = 0; i < answer.size(); i++) {
+            System.out.println("<div class=\"problem-box\">");
+            System.out.println("<h2>"+problem.get(i)+"</h2>");
+            System.out.println("<span>"+answer.get(i)+"</span>");
+            System.out.println("</div>");
+        }
     }
 }
--- web/index.jsp
+++ web/index.jsp
@@ -1,10 +1,3 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: Gabe
-  Date: 2020/10/26
-  Time: 14:34
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
